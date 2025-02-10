acd8230025de7010c77eabbb25bde5c10e72d50c
agpenamx <agpenamx@gmail.com>
2025-02-10 15:44:45 -0500

    Added Register.jsx and missing assets

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,21 @@
-import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import RegisterPanel from "./components/Register/Register"; 
+import Home from "./components/Home/Home"; 
 
 function App() {
   return (
     <Routes>
+      {/* Home Page - Detects if user is logged in */}
+      <Route path="/" element={<Home />} />
+
+      {/* Login Page */}
       <Route path="/login" element={<LoginPanel />} />
+
+      {/* Registration Page */}
+      <Route path="/register" element={<RegisterPanel />} />
     </Routes>
   );
 }
+
 export default App;
